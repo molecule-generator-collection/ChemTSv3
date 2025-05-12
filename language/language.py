f8a7b9a854bc8788ec4406bf1b5f4c92cbfa40d1
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-12 21:35:29 +0900

    merged abstract classes to one file for readability

--- language/language.py
+++ language/language.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
-from rdkit.Chem import Mol
 import torch
+from rdkit.Chem import Mol
+from collections import Counter
 
 
 #vocabulary can be dynamic for better compatibility, thus most methods are not static
@@ -81,3 +82,40 @@ class Language(ABC):
   
   def unk_tensor(self):
     return Language.list2tensor([self.unk_id()])
+
+#language that makes vocabulary from dataset
+class DynamicLanguage(Language):
+  def __init__(self):
+    self._vocab: list[str] = []
+    self._token2id = {}
+    self._id2token = {}
+
+  #split sentence to token strs, should include bos and eos
+  @abstractmethod
+  def sentence2tokens(self, sentence: str) -> list[str]:
+    raise NotImplementedError("sentence2tokens needs to be implemented.")
+  
+  #can input dataset
+  def build_vocab(self, splits: dict[str, list[dict]]):
+    counter = Counter()
+    for split_name, examples in splits.items():
+        for ex in examples:
+            tokens = self.sentence2tokens(ex["text"])
+            counter.update(tokens)
+    self._vocab = sorted(counter.keys())
+    self._vocab.append(self.pad_token())
+    self._vocab.append(self.unk_token())
+    self._token2id = {tok: idx for idx, tok in enumerate(self._vocab)}
+    self._id2token = {idx: tok for tok, idx in self._token2id.items()}
+
+  #override
+  def vocab(self):
+    return self._vocab
+
+  #override
+  def token2id(self, token):
+    return self._token2id.get(token, self._token2id[self.unk_token()])
+
+  #override
+  def id2token(self, tokenid):
+    return self._id2token[tokenid]
