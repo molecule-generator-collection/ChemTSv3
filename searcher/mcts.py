e243842af757113341b27cf842e2eda2382494fc
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-13 11:23:53 +0900

    added dummy_reward option

--- searcher/mcts.py
+++ searcher/mcts.py
@@ -67,10 +67,11 @@ class MCTS():
       node.mean_r = node.sum_r / node.n
       node = node.parent
 
-  def search(self, root: Node, policy: Type[Policy], policy_conf: dict = None, expansion_threshold=None, rollout_threshold=None, exhaust_backpropagate=False, max_rollouts=None, time_limit=None, max_generations=None):
+  def search(self, root: Node, policy: Type[Policy], policy_conf: dict = None, expansion_threshold=None, rollout_threshold=None, exhaust_backpropagate=False, dummy_reward=False, max_rollouts=None, time_limit=None, max_generations=None):
     #exhaust_backpropagate: whether to backpropagate or not when every terminal node under the node is already explored (only once: won't be visited again)
     #expansion_threshold: [0-1], ignore children with low transition probabilities in expansion based on this value
     #rollout_threshold: [0-1], ignore children with low transition probabilities in rollout based on this value, set to the same value as expansion_threshold by default
+    #dummy_reward: backpropagate value is fixed to 0, still calculates rewards and objective values
     
     assert (max_rollouts is not None) or (time_limit is not None) or (max_generations is not None), \
         "specify at least one of max_genrations, max_rollouts or time_limit"
@@ -111,11 +112,15 @@ class MCTS():
             self.logging("!------exhaust every terminal under: " + str(node.parent) + "------!")
           if exhaust_backpropagate:
             value = self._eval(node)
+            if dummy_reward:
+              value = 0
             self._backpropagate(node, value)
           node.parent.sum_r = node.parent.mean_r = -float("inf")
           node = root
           continue
       value = self._eval(node)
+      if dummy_reward:
+        value = 0
       self._backpropagate(node, value)
 
     print("Search is completed.")
