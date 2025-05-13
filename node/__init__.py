512815c85291a82a09e69eae1dafe0ced85065e1
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-14 08:57:37 +0900

    pep8

--- node/__init__.py
+++ node/__init__.py
@@ -1,4 +1,4 @@
 from .node import Node, MolNode
-from .sentencenode import SentenceNode, MolSentenceNode
+from .sentence_node import SentenceNode, MolSentenceNode
 
 __all__ = ["Node", "MolNode", "SentenceNode", "MolSentenceNode"]
