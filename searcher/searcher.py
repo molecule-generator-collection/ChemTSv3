ed611b10226667f221510b8a9e6c5776f11c37a8
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-13 12:46:15 +0900

    added base searcher class

--- searcher/searcher.py
+++ searcher/searcher.py
@@ -1,6 +1,14 @@
 from abc import ABC, abstractmethod
+import time, datetime
 
 class searcher(ABC):
-  @abstractmethod
+  def __init__(self, name=None):
+    self._name = name
+    self._name = self.name() #generate name if name=None
+
   def name(self):
-    pass
+    if self._name is not None:
+      return self._name
+    else:
+      return str(datetime.datetime.now())
+  
