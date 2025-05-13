ff09a7007c3adb05272825fd11b833129f9a754a
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-13 09:21:29 +0900

    added default returns to confs

--- policy/ucb.py
+++ policy/ucb.py
@@ -3,10 +3,11 @@ from .policy import Policy
 from node.node import Node
 
 class UCB(Policy):
-  def evaluate(node: Node, conf={"c":1}):
+  def evaluate(node: Node, conf: dict):
+    #c: exploration parameter, default: 1
     if node.parent is None:
       return node.mean_r
     if node.n == 0:
       return float("inf")
-    u = conf["c"] * sqrt(2 * log(node.parent.n) / (node.n))
+    u = conf.get("c", 1) * sqrt(2 * log(node.parent.n) / (node.n))
     return node.mean_r + u
