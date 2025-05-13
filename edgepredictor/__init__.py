2c818b091d96c03003b33742aa13d71426fb3922
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-14 08:57:37 +0900

    pep8

--- edgepredictor/__init__.py
+++ edgepredictor/__init__.py
@@ -1,4 +1,4 @@
-from .edgepredictor import EdgePredictor, LanguageModel
+from .edge_predictor import EdgePredictor, LanguageModel
 from .gpt2lm import GPT2LM
 
 __all__ = ["EdgePredictor", "LanguageModel", "GPT2LM"]
