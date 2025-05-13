6b798b41cfa699e6cea023e960b49a49cebbbddd
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-13 11:46:00 +0900

    added name()

--- searcher/mcts.py
+++ searcher/mcts.py
@@ -6,15 +6,18 @@ from scipy import stats
 from node.node import Node
 from edgepredictor.edgepredictor import EdgePredictor
 from policy.policy import Policy
+from policy.policy import UCB
 from reward.reward import Reward
 from reward.logp_reward import LogP_reward
 
 class MCTS():
-  def __init__(self, edgepredictor: EdgePredictor, rewardfunc: Type[Reward] = LogP_reward, reward_conf: dict = None, rollout_limit=4096, print_output=True, verbose=False, name=None):
+  def __init__(self, edgepredictor: EdgePredictor, rewardfunc: Type[Reward] = LogP_reward, reward_conf: dict = None, policy: Type[Policy] = UCB, policy_conf: dict = None, rollout_limit=4096, print_output=True, verbose=False, name=None):
     self.edgepredictor = edgepredictor
     self.rewardfunc = rewardfunc
     self.reward_conf = reward_conf or {}
       #null_reward ...  reward for invalid molecules, default: 1
+    self.policy = policy
+    self.policy_conf = policy_conf or {}
     self.rollout_limit = rollout_limit
     self.record: dict[str, tuple[list[float], float]] = {}
     self.unique_molkeys = []
@@ -23,13 +26,21 @@ class MCTS():
     self.times = []
     self.print_output = print_output
     self.verbose = verbose
-    self.name = name
-    self.filename = self.name
+    self._name = name or self.name()
+    self.filename = self._name    
     self.count_rollouts = 0
     self.passed_time = 0
     #for search
     self.expansion_threshold = 0.995
     self.rollout_threshold = 0.995
+    
+  def name(self):
+    if self.name is not None:
+      return self.name
+    else:
+      policy_name = self.policy.__class__.__name__
+      policy_c = str(self.policy_conf.get("c", 1))
+      return policy_name + ", c = " + policy_c + ", " + str(datetime.datetime.now())
 
   def _expand(self, node: Node):
     if node.is_terminal():
@@ -67,7 +78,7 @@ class MCTS():
       node.mean_r = node.sum_r / node.n
       node = node.parent
 
-  def search(self, root: Node, policy: Type[Policy], policy_conf: dict = None, expansion_threshold=None, rollout_threshold=None, exhaust_backpropagate=False, dummy_reward=False, max_rollouts=None, time_limit=None, max_generations=None):
+  def search(self, root: Node, expansion_threshold=None, rollout_threshold=None, exhaust_backpropagate=False, dummy_reward=False, max_rollouts=None, time_limit=None, max_generations=None):
     #exhaust_backpropagate: whether to backpropagate or not when every terminal node under the node is already explored (only once: won't be visited again)
     #expansion_threshold: [0-1], ignore children with low transition probabilities in expansion based on this value
     #rollout_threshold: [0-1], ignore children with low transition probabilities in rollout based on this value, set to the same value as expansion_threshold by default
@@ -82,10 +93,6 @@ class MCTS():
         self.rollout_threshold = expansion_threshold
     if rollout_threshold is not None:
       self.rollout_threshold = rollout_threshold
-
-    if self.name is None:
-      self.filename = self.name = str(datetime.datetime.now())
-      #old: self.name = policy + "_c" + '{:.2f}'.format(c) + "_" + str(datetime.datetime.now())
 
     time_start = time.time()
     initial_time = self.passed_time
@@ -106,7 +113,7 @@ class MCTS():
 
       node = root
       while node.children:
-        node = max(node.children.values(), key=lambda n: policy.evaluate(n, conf=policy_conf))
+        node = max(node.children.values(), key=lambda n: self.policy.evaluate(n, conf=self.policy_conf))
         if node.sum_r == -float("inf"): #already exhausted every terminal under this
           if self.verbose:
             self.logging("!------exhaust every terminal under: " + str(node.parent) + "------!")
