1a48861e71ff8301d60dcca6c818f4d2f9a8cc70
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-14 17:25:56 +0900

    abstract classes are packed in base.py of each package

--- edge_predictor/__init__.py
+++ edge_predictor/__init__.py
@@ -1,4 +1,4 @@
-from .edge_predictor import EdgePredictor, LanguageModel
+from .base import EdgePredictor, LanguageModel
 from .gpt2 import GPT2EdgePredictor
 
 __all__ = ["EdgePredictor", "LanguageModel", "GPT2EdgePredictor"]
--- node/__init__.py
+++ node/__init__.py
@@ -1,4 +1,4 @@
-from .node import Node, MolNode
+from .base import Node, MolNode
 from .sentence_node import SentenceNode, MolSentenceNode
 
 __all__ = ["Node", "MolNode", "SentenceNode", "MolSentenceNode"]
