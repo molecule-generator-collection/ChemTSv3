fa0177c9e65b7cebc6eacedd6c08b0d6843759cd
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-05-14 11:39:02 +0900

    dummy_reward -> use_dummy_reward, and now passed to _backpropagate

--- searcher/mcts.py
+++ searcher/mcts.py
@@ -58,19 +58,22 @@ class MCTS(Searcher):
     objective_values, reward = self._rollout(node)
     return reward
 
-  def _rollout(self, node):
+  def _rollout(self, node: Node):
     if node.id_tensor.numel() >= self.rollout_limit:
       return self.reward_conf.get("null_reward", -1)
     mol = self.edge_predictor.generate(node, conf={"rollout_threshold": self.rollout_threshold})
     self.count_rollouts += 1
     return self.grab_objective_values_and_reward(mol)
 
-  def _backpropagate(self, node, value):
+  def _backpropagate(self, node: Node, value: float, use_dummy_reward: bool):
     while node:
-      node.observe(value)
+      if use_dummy_reward:
+        node.observe(0)
+      else:
+        node.observe(value)
       node = node.parent
 
-  def search(self, root: Node=None, expansion_threshold=None, rollout_threshold=None, exhaust_backpropagate=False, dummy_reward=False, max_rollouts=None, time_limit=None, max_generations=None, change_root=False):
+  def search(self, root: Node=None, expansion_threshold=None, rollout_threshold=None, exhaust_backpropagate=False, use_dummy_reward=False, max_rollouts=None, time_limit=None, max_generations=None, change_root=False):
     #exhaust_backpropagate: whether to backpropagate or not when every terminal node under the node is already explored (only once: won't be visited again)
     #expansion_threshold: [0-1], ignore children with low transition probabilities in expansion based on this value
     #rollout_threshold: [0-1], ignore children with low transition probabilities in rollout based on this value, set to the same value as expansion_threshold by default
@@ -120,16 +123,12 @@ class MCTS(Searcher):
             self.logging("!------exhaust every terminal under: " + str(node.parent) + "------!")
           if exhaust_backpropagate:
             value = self._eval(node)
-            if dummy_reward:
-              value = 0
-            self._backpropagate(node, value)
+            self._backpropagate(node, value, use_dummy_reward)
           node.parent.sum_r = node.parent.mean_r = -float("inf")
           node = self.root
           continue
       value = self._eval(node)
-      if dummy_reward:
-        value = 0
-      self._backpropagate(node, value)
+      self._backpropagate(node, value, use_dummy_reward)
 
     self.plot_everything(x_axis = "generation_order", maxline = True)
     self.plot_everything(x_axis = "time", maxline = True)
