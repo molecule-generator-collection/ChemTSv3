c2a2eee83d58a70c95309fa4ccc683b1753a9ee9
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-03 22:11:47 +0900

    implemented / tested lipinski filter

---
+++ filter/lipinski_filter.py
@@ -0,0 +1,38 @@
+from rdkit.Chem import Mol, Descriptors, rdMolDescriptors
+from filter import MolFilter
+
+class LipinskiFilter(MolFilter):
+    def __init__(self, rule_of: int = None, max_mol_weight = None, max_logP = None, max_hydrogen_bond_donors = None, max_hydrogen_bond_acceptors = None, max_rotatable_bonds = None):
+        """
+        Prioritize max_*** over rule_of value.
+        """
+        if not (rule_of is None or rule_of == 3 or rule_of == 5):
+            raise AssertionError("rule_of must be either 5, 3, or None")
+        
+        self.max_hydrogen_bond_acceptors = self.max_mol_weight = self.max_rotatable_bonds = None
+        if rule_of:
+            self.max_mol_weight = 500 * rule_of
+            if rule_of == 3:
+                self.max_hydrogen_bond_acceptors = 3
+                self.max_rotatable_bonds = 3
+            elif rule_of == 5:
+                self.max_hydrogen_bond_acceptors = 10
+                
+        self.max_logP = max_logP or rule_of or float("inf")
+        self.max_hydrogen_bond_donors = max_hydrogen_bond_donors or rule_of or float("inf")
+        self.max_hydrogen_bond_acceptors = max_hydrogen_bond_acceptors or self.max_hydrogen_bond_acceptors or float("inf")
+        self.max_mol_weight = max_mol_weight or self.max_mol_weight or float("inf")
+        self.max_rotatable_bonds = max_rotatable_bonds or self.max_rotatable_bonds or float("inf")
+
+    #implement
+    def mol_check(self, mol: Mol) -> bool:
+        mol_weight = round(rdMolDescriptors._CalcMolWt(mol), 2)
+        logP = Descriptors.MolLogP(mol)
+        n_hydrogen_bond_donors = rdMolDescriptors.CalcNumLipinskiHBD(mol)
+        n_hydrogen_bond_acceptors = rdMolDescriptors.CalcNumLipinskiHBA(mol)
+        n_rotatable_bonds = rdMolDescriptors.CalcNumRotatableBonds(mol)
+        return (mol_weight <= self.max_mol_weight
+                and logP <= self.max_logP
+                and n_hydrogen_bond_donors <= self.max_hydrogen_bond_donors
+                and n_hydrogen_bond_acceptors <= self.max_hydrogen_bond_acceptors
+                and n_rotatable_bonds <= self.max_rotatable_bonds)
