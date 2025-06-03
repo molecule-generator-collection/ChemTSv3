dad79164ca66f6ad7c42c7ce5cfc182346a6de20
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-03 23:42:50 +0900

    yaml uses class name instead of class path

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
-from .class_utils import get_class_from_class_path
+from .class_utils import get_class_from_class_path, get_class_from_package
 from .math_utils import select_indices_by_threshold
 from .helm_utils import MonomersLib, HELMConverter
 from .mol_utils import is_same_mol, get_main_mol, remove_isotopes, print_atoms_and_labels, draw_mol
 
-__all__ = ["get_class_from_class_path", "select_indices_by_threshold", "MonomersLib", "HELMConverter", "is_same_mol", "get_main_mol", "remove_isotopes", "print_atoms_and_labels", "draw_mol"]
+__all__ = ["get_class_from_class_path", "get_class_from_package", "select_indices_by_threshold", "MonomersLib", "HELMConverter", "is_same_mol", "get_main_mol", "remove_isotopes", "print_atoms_and_labels", "draw_mol"]
--- utils/class_utils.py
+++ utils/class_utils.py
@@ -7,6 +7,10 @@ def get_class_from_class_path(class_path: str):
     module = importlib.import_module(module_path)
     return getattr(module, class_name)
 
+def get_class_from_package(package_name: str, class_name: str):
+    module = importlib.import_module(package_name)
+    return getattr(module, class_name)
+
 def get_class_path_from_object(obj: object) -> str:
     cls = obj.__class__
     return f"{cls.__module__}.{cls.__name__}"
