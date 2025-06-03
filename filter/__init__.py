c439bd3b8d651aeef91b47fd4463e607fb5aac37
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-03 22:11:47 +0900

    implemented / tested lipinski filter

--- filter/__init__.py
+++ filter/__init__.py
@@ -1,4 +1,5 @@
 from .base import Filter, MolFilter
+from .lipinski_filter import LipinskiFilter
 from .validity_filter import ValidityFilter
 
-__all__ = ["Filter", "MolFilter", "ValidityFilter"]
+__all__ = ["Filter", "MolFilter", "LipinskiFilter", "ValidityFilter"]
--- filter/validity_filter.py
+++ filter/validity_filter.py
@@ -1,11 +1,7 @@
-from rdkit.Chem import Mol, Descriptors
+from rdkit.Chem import Mol
 from filter import MolFilter
-from node import MolNode
 
 class ValidityFilter(MolFilter):
-    def __init__(self, **kwargs):
-        pass
-
     #implement
     def mol_check(self, mol: Mol) -> bool:
         if mol is None or mol.GetNumAtoms()==0:
