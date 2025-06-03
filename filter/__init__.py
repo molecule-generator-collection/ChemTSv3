d78551fb0d5e789dbf396de071791075e56eb4eb
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-03 22:44:26 +0900

    added LogPFilter / improved LipinskiFilter code

--- filter/__init__.py
+++ filter/__init__.py
@@ -1,5 +1,6 @@
 from .base import Filter, MolFilter
 from .lipinski_filter import LipinskiFilter
+from .logP_filter import LogPFilter
 from .validity_filter import ValidityFilter
 
-__all__ = ["Filter", "MolFilter", "LipinskiFilter", "ValidityFilter"]
+__all__ = ["Filter", "MolFilter", "LipinskiFilter", "LogPFilter", "ValidityFilter"]
