0e82826694349b7dc2e4684e497534a07f680e0b
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-04 20:50:15 +0900

    implemented ValueFilter, MolValueFilter base class / fixed moving average error with low generation count

--- filter/attachment_points_filter.py
+++ filter/attachment_points_filter.py
@@ -1,12 +1,12 @@
 from rdkit.Chem import Mol, Descriptors
-from filter import Filter
+from filter import ValueFilter
 from node import SentenceNode
 
-class AttachmentPointsFilter(Filter):
-    def __init__(self, n=2):
-        self.n = n
+class AttachmentPointsFilter(ValueFilter):
+    def __init__(self, allowed=2, **kwargs):
+        super().__init__(allowed=allowed, **kwargs)
         
     #implement
     def check(self, node: SentenceNode) -> bool:
         smiles = str(node)
-        return smiles.count("*") == self.n
+        return smiles.count("*")
