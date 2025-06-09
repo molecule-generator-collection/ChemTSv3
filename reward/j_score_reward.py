aa48d1c303ad23bc9da916569b1de1d30d5deaeb
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-09 15:11:06 +0900

    const capitalization

--- reward/j_score_reward.py
+++ reward/j_score_reward.py
@@ -6,21 +6,19 @@ from utils.third_party import sascorer
 # edited from ChemTSv2
 # ref: https://github.com/tsudalab/ChemTS/blob/4174c3600ebb47ed136b433b22a29c879824a6ba/mcts_logp_improved_version/add_node_type.py#L172
 
+LOG_P_BASELINE = np.loadtxt("../data/misc/j_score/logP_values.txt")
+LOG_P_MEAN = np.mean(LOG_P_BASELINE)
+LOG_P_STD = np.std(LOG_P_BASELINE)
+
+SA_BASELINE = np.loadtxt("../data/misc/j_score/SA_scores.txt")
+SA_MEAN = np.mean(SA_BASELINE)
+SA_STD = np.std(SA_BASELINE)
+
+CA_BASELINE = np.loadtxt("../data/misc/j_score/cycle_scores.txt")
+CS_MEAN = np.mean(CA_BASELINE)
+CS_STD = np.std(CA_BASELINE)
+
 class JScoreReward(MolReward):
-    def __init__(self, **kwargs):
-        log_p_baseline = np.loadtxt("../data/misc/j_score/logP_values.txt")
-        self.log_p_mean = np.mean(log_p_baseline)
-        self.log_p_std = np.std(log_p_baseline)
-
-        sa_baseline = np.loadtxt("../data/misc/j_score/SA_scores.txt")
-        self.sa_mean = np.mean(sa_baseline)
-        self.sa_std = np.std(sa_baseline)
-
-        cs_baseline = np.loadtxt("../data/misc/j_score/cycle_scores.txt")
-        self.cs_mean = np.mean(cs_baseline)
-        self.cs_std = np.std(cs_baseline)
-        super().__init__(**kwargs)
-         
     #implement
     def mol_objective_functions(self):
         def log_p(mol):
@@ -39,9 +37,9 @@ class JScoreReward(MolReward):
     #implement
     def reward_from_objective_values(self, values):
         logP, sascore, ring_size_penalty = values
-        logP_norm = (logP - self.log_p_mean) / self.log_p_std
-        sascore_norm = (-sascore - self.sa_mean) / self.sa_std
-        rs_penalty_norm = (-ring_size_penalty - self.cs_mean) / self.cs_std
+        logP_norm = (logP - LOG_P_MEAN) / LOG_P_STD
+        sascore_norm = (-sascore - SA_MEAN) / SA_STD
+        rs_penalty_norm = (-ring_size_penalty - CS_MEAN) / CS_STD
         # jscore = logP - sascore - ring_size_penalty
         jscore = logP_norm + sascore_norm + rs_penalty_norm
         return jscore / (1 + abs(jscore))
