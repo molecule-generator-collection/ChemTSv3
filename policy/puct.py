f24a59b658f4ac998f058ee77ae41e18398b91ab
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-09 12:36:02 +0900

    added initial_mean option

--- policy/puct.py
+++ policy/puct.py
@@ -7,19 +7,20 @@ from utils import make_curve_from_points
 class PUCT(Policy):
     c: Callable[[float], float] #depth -> exploration parameter
     forced_rollout: bool #whether to return inf score for unexplored node or not
+    initial_mean: float
     
-    def __init__(self, c: Callable[[float], float] | list[tuple[float, float]] | float=1, forced_rollout: bool=True):
+    def __init__(self, c: Callable[[float], float] | list[tuple[float, float]] | float=1, initial_mean = float("inf")):
         if type(c) == Callable:
             self.c = c
         elif type(c) == list:
             self.c = make_curve_from_points(c)
         else:
             self.c = lambda x: c
-        self.forced_rollout = forced_rollout
+        self.initial_mean = initial_mean
         
     #implement
     def evaluate(self, node: Node):
-        if node.n == 0 and self.forced_rollout:
-            return float("inf")
         u = self.c(node.depth) * node.last_prob * sqrt(node.parent.n) / (1 + node.n)
+        if node.n == 0:
+            return self.initial_mean + u
         return node.mean_r + u
--- policy/ucb.py
+++ policy/ucb.py
@@ -7,18 +7,19 @@ from utils import make_curve_from_points
 class UCB(Policy):
     c: Callable[[float], float] #depth -> exploration parameter
     
-    def __init__(self, c: Callable[[float], float] | list[tuple[float, float]] | float=1):
+    def __init__(self, c: Callable[[float], float] | list[tuple[float, float]] | float=1, initial_mean = float("inf")):
         if type(c) == Callable:
             self.c = c
         elif type(c) == list:
             self.c = make_curve_from_points(c)
         else:
             self.c = lambda x: c
+        self.initial_mean = initial_mean
     
     #implement
     def evaluate(self, node: Node):
         #c: exploration parameter
-        if node.n == 0:
-            return float("inf")
         u = self.c(node.depth) * sqrt(2 * log(node.parent.n) / (node.n))
+        if node.n == 0:
+            return self.initial_mean + u
         return node.mean_r + u
