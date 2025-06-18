580660976c7f03977680f122fab2749efd954c93
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-18 15:12:17 +0900

    added use_filtered_reward and count_filtered_node_as_valid

--- filter/validity_filter.py
+++ filter/validity_filter.py
@@ -4,6 +4,9 @@ from rdkit.Chem import Mol
 from filter import MolFilter
 
 class ValidityFilter(MolFilter):
+    def __init__(filtered_reward_override: float=None, regard_filtered_node_as_valid: bool=False):
+        super().__init__(filtered_reward_override=filtered_reward_override, regard_filtered_node_as_valid=regard_filtered_node_as_valid)
+        
     # implement
     def mol_check(self, mol: Mol) -> bool:
         if mol is None or mol.GetNumAtoms()==0:
