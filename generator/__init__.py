4914605a473514a0ff5cee1c3db9aa6139d8150e
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-18 11:11:21 +0900

    implemented multiprocess mcts skeleton

--- generator/__init__.py
+++ generator/__init__.py
@@ -1,5 +1,6 @@
 from .base import Generator
 from .mcts import MCTS
+from .multiprocess_mcts import MultiProcessMCTS
 from .random_generator import RandomGenerator
 
-__all__ = ["Generator", "MCTS", "RandomGenerator"]
+__all__ = ["Generator", "MCTS", "MultiProcessMCTS", "RandomGenerator"]
