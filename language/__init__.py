2070e48a143867b0ba5b6d175bfb79a052385547
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-06-30 17:22:27 +0900

    Add SELFIES class

--- language/__init__.py
+++ language/__init__.py
@@ -1,5 +1,6 @@
 from .base import Language, DynamicLanguage, MolLanguage, DynamicMolLanguage
 from .helm import HELM
+from .selfies import SELFIES
 from .smiles import SMILES
 
-__all__ = ["Language", "DynamicLanguage", "MolLanguage", "DynamicMolLanguage", "HELM", "SMILES"]
+__all__ = ["Language", "DynamicLanguage", "MolLanguage", "DynamicMolLanguage", "HELM", "SELFIES", "SMILES"]
