6908fd7691896dc0c2bb3ffe408eb4b3c62976c2
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-01 17:05:54 +0900

    Add BioT5Transition / Fix BlackBoxTransition not reflecting last_action and last_prob

--- transition/__init__.py
+++ transition/__init__.py
@@ -1,5 +1,6 @@
-from .base import Transition, LanguageModel
+from .base import Transition, LanguageModel, BlackBoxTransition
+from .biot5 import BioT5Transition
 from .gpt2 import GPT2Transition
 from .rnn import RNNLanguageModel, RNNTransition
 
-__all__ = ["Transition", "LanguageModel", "GPT2Transition", "RNNLanguageModel", "RNNTransition"]
+__all__ = ["Transition", "LanguageModel", "BlackBoxTransition", "BioT5Transition", "GPT2Transition", "RNNLanguageModel", "RNNTransition"]
