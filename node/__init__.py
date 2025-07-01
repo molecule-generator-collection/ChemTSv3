33b163c09ead12c7be787ad8fc69f75e24de2fc3
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-01 14:26:10 +0900

    Add MolStringNode

--- node/__init__.py
+++ node/__init__.py
@@ -1,4 +1,5 @@
 from .base import Node, MolNode, SurrogateNode
 from .sentence_node import SentenceNode, MolSentenceNode
+from .mol_string_node import MolStringNode
 
-__all__ = ["Node", "MolNode", "SurrogateNode", "SentenceNode", "MolSentenceNode"]
+__all__ = ["Node", "MolNode", "SurrogateNode", "SentenceNode", "MolSentenceNode", "MolStringNode"]
