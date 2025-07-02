c670b1ee3567ce2928603f94c0213c71581ba6d4
satoru-fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-02 15:23:33 +0900

    Remove __all__

--- generator/__init__.py
+++ generator/__init__.py
@@ -2,5 +2,3 @@ from .base import Generator
 from .mcts import MCTS
 from .multiprocess_mcts import MultiProcessMCTS
 from .random_generator import RandomGenerator
-
-__all__ = ["Generator", "MCTS", "MultiProcessMCTS", "RandomGenerator"]
--- policy/__init__.py
+++ policy/__init__.py
@@ -1,5 +1,3 @@
 from .base import Policy, ValuePolicy
 from .ucb1 import UCB1
 from .puct import PUCT
-
-__all__ = ["Policy", "ValuePolicy", "UCB1", "PUCT"]
