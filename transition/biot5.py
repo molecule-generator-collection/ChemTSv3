4c6af0d5288a5b71d5639aae108fce6edb734a72
satoru-fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-02 18:06:08 +0900

    Update prompt

--- transition/biot5.py
+++ transition/biot5.py
@@ -9,14 +9,15 @@ MODEL = AutoModelForSeq2SeqLM.from_pretrained("QizhiPei/biot5-base-text2mol")
 print("Model loading completed.")
 
 class BioT5Transition(BlackBoxTransition):
-    def __init__(self, target_objective: str, prompt_prefix: str=None, n_samples=2, logger: logging.Logger=None):
+    def __init__(self, target_objective: str, prompt_prefix: str=None, prompt_postfix: str=None, n_samples=2, logger: logging.Logger=None):
         self.target_objective = target_objective
-        self.prompt_prefix = prompt_prefix or "Definition: You are given a molecule SELFIES. Your job is to generate a SELFIES molecule that "
+        self.prompt_prefix = prompt_prefix or "Definition: You are given a molecule SELFIES. Your job is to generate a SELFIES molecule that"
+        self.prompt_postfix = prompt_postfix or ""
         super().__init__(logger=logger)
         
     def sample_transition(self, node: MolStringNode) -> MolStringNode:
         sel = node.string
-        prompt = self.prompt_prefix + self.target_objective + ". Now complete the following example - Input: <bom>" + sel + "<eom> Output: "
+        prompt = self.prompt_prefix + " " + self.target_objective + self.prompt_postfix + ". Now complete the following example - Input: <bom>" + sel + "<eom> Output: "
         
         input_ids = TOKENIZER(prompt, return_tensors="pt").input_ids
         outputs = MODEL.generate(input_ids, max_length=512, do_sample=True)
