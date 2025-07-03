d10b953a3b9ce12eb3513780452373575d9059af
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-03 13:20:08 +0900

    Change prompt template

--- transition/biot5.py
+++ transition/biot5.py
@@ -12,12 +12,12 @@ class BioT5Transition(BlackBoxTransition):
     def __init__(self, target_objective: str, prompt_prefix: str=None, prompt_postfix: str=None, n_samples=2, logger: logging.Logger=None):
         self.target_objective = target_objective
         self.prompt_prefix = prompt_prefix or "Definition: You are given a molecule SELFIES. Your job is to generate a SELFIES molecule that"
-        self.prompt_postfix = prompt_postfix or ""
+        self.prompt_postfix = prompt_postfix or ". Now complete the following example"
         super().__init__(logger=logger)
         
     def sample_transition(self, node: MolStringNode) -> MolStringNode:
         sel = node.string
-        prompt = self.prompt_prefix + " " + self.target_objective + self.prompt_postfix + ". Now complete the following example - Input: <bom>" + sel + "<eom> Output: "
+        prompt = self.prompt_prefix + " " + self.target_objective + self.prompt_postfix + " - Input: <bom>" + sel + "<eom> Output: "
         
         input_ids = TOKENIZER(prompt, return_tensors="pt").input_ids
         outputs = MODEL.generate(input_ids, max_length=512, do_sample=True)
