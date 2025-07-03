4e64fe29768d86d6cdb957357bebb95989221310
satoru-fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-03 15:45:14 +0900

    Fix circular import / Use self.logger

--- transition/biot5_plus.py
+++ transition/biot5_plus.py
@@ -7,14 +7,14 @@ from transition import BlackBoxTransition
 class BioT5PlusTransition(BlackBoxTransition):
     def __init__(self, target_objective: str, prompt_prefix: str=None, prompt_postfix: str=None, n_samples=2, logger: logging.Logger=None):
         self.target_objective = target_objective
-        self.prompt_prefix = prompt_prefix or "The molecule is obtained by performing a single reaction on: "
-        self.prompt_postfix = prompt_postfix or ". The output molecule must be connected."
+        self.prompt_prefix = prompt_prefix or "The molecule is obtained by performing a single molecular editing (such as amide bond formation, Suzuki–Miyaura coupling, and SNAr reactions) on: "
+        self.prompt_postfix = prompt_postfix or ". The output molecule must be connected (must not have '.' in selfies)."
         super().__init__(logger=logger)
         
-        logger.info("Loading BioT5+ models...")
+        self.logger.info("Loading BioT5+ models...")
         self.tokenizer = AutoTokenizer.from_pretrained("QizhiPei/biot5-plus-base-mol-instructions-molecule")
         self.model = T5ForConditionalGeneration.from_pretrained('QizhiPei/biot5-plus-base-mol-instructions-molecule')
-        logger.info("Model loading completed.")
+        self.logger.info("Model loading completed.")
         
     def sample_transition(self, node: MolStringNode) -> MolStringNode:
         parent_string = node.string
