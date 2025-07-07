821a6590c9831e7e8642ee53e9a9ff4a340fd95d
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-07 16:08:25 +0900

    Added default SMILES / SELFIES string node classes

--- node/__init__.py
+++ node/__init__.py
@@ -1,3 +1,10 @@
 from .base import Node, MolNode, SurrogateNode
 from .sentence_node import SentenceNode, MolSentenceNode
-from .mol_string_node import MolStringNode
+from .mol_string_node import MolStringNode, SMILESStringNode
+
+# lazy import
+def __getattr__(name):
+    if name == "SELFIESStringNode":
+        from .mol_string_node import SELFIESStringNode
+        return SELFIESStringNode
+    raise AttributeError(f"module {__name__} has no attribute {name}")
