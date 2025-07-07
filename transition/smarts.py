f473ec1b6365806594ea6bfebb4675d8cb6bc190
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-07 15:51:28 +0900

    Test rule-based transition

---
+++ transition/smarts.py
@@ -0,0 +1,7 @@
+from node import MolStringNode
+from transition import Transition
+
+class SmartsTransition(Transition):
+    # implement
+    def transitions_with_probs(self, node: MolStringNode):
+        pass
