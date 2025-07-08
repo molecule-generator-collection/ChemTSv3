707fd5f3484c9a35eb943872482f166f4119287a
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-08 14:11:12 +0900

    Use .inf in yaml

--- generator/mcts.py
+++ generator/mcts.py
@@ -13,7 +13,7 @@ class MCTS(Generator):
 
         Args:
             root: The root node. Use SurrogateNode to search from multiple nodes.
-            eval_width: The number of children to sample during eval step. To perform evals for all children, set this to a value higher than the number of tokens.
+            eval_width: The number of children to sample during eval step. To evaluate all children, set this to float("inf") in Python code or .inf in YAML.
             allow_eval_overlaps: whether to allow overlap nodes when sampling eval candidates (recommended: False)
             n_evals: the number of child node evaluations (rollouts for children that has_reward = False)
             n_tries: the number of attempts to obtain an unfiltered node in a single eval (should be 1 unless has_reward() can be False or filters are probabilistic)
