9feafcda91aa210156e1c33d9da1c7b42e54bfa9
satoru-fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-08 17:52:00 +0900

    Add exploration parameter to actions

--- policy/uct_with_action_prior.py
+++ policy/uct_with_action_prior.py
@@ -1,18 +1,22 @@
+from math import log, sqrt
 from typing import Any, Callable
 from node import Node
 from policy import UCT
     
 class UCTAP(UCT):
     """UCT with Action Prior"""
-    def __init__(self, c: Callable[[float], float] | list[tuple[float, float]] | float=1, best_rate: float=0.0, prior_offset: float=0.2, prior_weight: int=1, max_prior: float=None, use_parent_reward: bool=True):
+    def __init__(self, c: Callable[[float], float] | list[tuple[float, float]] | float=1, best_rate: float=0.0, action_c:float=1.0, prior_offset: float=0.2, prior_weight: int=1, max_prior: float=None, use_parent_reward: bool=True):
+        self.sum_action_n = 0
         self.action_n = {}
         self.action_sum_r = {}
+        self.action_c = action_c
         self.prior_offset = prior_offset
         self.use_parent_reward = use_parent_reward
         super().__init__(c=c, best_rate=best_rate, prior_weight=prior_weight, max_prior=max_prior)
 
     # override
     def observe(self, child: Node, objective_values: list[float], reward: float):
+        self.sum_action_n += 1
         action = child.last_action
         if type(action) == tuple:
             action = action[0]
@@ -37,6 +41,7 @@ class UCTAP(UCT):
             return 0, 0
         else:
             prior = self.action_sum_r[last_action] / self.action_n[last_action]
+            prior += self.action_c * sqrt(log(self.sum_action_n) / (self.action_n[last_action]))
             prior += self.prior_offset
             if self.use_parent_reward and node.parent.reward is not None:
                 prior += node.parent.reward
