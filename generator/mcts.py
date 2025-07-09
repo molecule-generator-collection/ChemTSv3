8a33a5872e7f73c686e1103e7d081a64b9b2370d
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-09 13:35:34 +0900

    Add reward_cutoff

--- generator/mcts.py
+++ generator/mcts.py
@@ -7,7 +7,7 @@ from reward import Reward, LogPReward
 from transition import Transition
 
 class MCTS(Generator):
-    def __init__(self, root: Node, transition: Transition, max_tree_depth=None, output_dir=None, name=None, reward: Reward=LogPReward(), policy: Policy=UCT(), filters: list[Filter]=None, filter_reward: float | str | list=0, failed_parent_reward: float | str="ignore", eval_width: int=1, allow_eval_overlaps: bool=False, n_evals: int=1, n_tries: int =1, cut_failed_child: bool=False, terminal_reward: float | str="ignore", freeze_terminal: bool=True, check_loop: bool=False, use_dummy_reward: bool=False, logger: logging.Logger=None, info_interval: int=100):
+    def __init__(self, root: Node, transition: Transition, max_tree_depth=None, output_dir=None, name=None, reward: Reward=LogPReward(), policy: Policy=UCT(), filters: list[Filter]=None, filter_reward: float | str | list=0, failed_parent_reward: float | str="ignore", eval_width: int=1, allow_eval_overlaps: bool=False, n_evals: int=1, n_tries: int =1, cut_failed_child: bool=False, reward_cutoff: float=None, terminal_reward: float | str="ignore", freeze_terminal: bool=True, check_loop: bool=False, use_dummy_reward: bool=False, logger: logging.Logger=None, info_interval: int=100):
         """
         Perform MCTS to maximize the reward.
 
@@ -18,12 +18,13 @@ class MCTS(Generator):
             n_evals: the number of child node evaluations (rollouts for children that has_reward = False)
             n_tries: the number of attempts to obtain an unfiltered node in a single eval (should be 1 unless has_reward() can be False or filters are probabilistic)
             filter_reward: Backpropagate this value when {n_tries} evals are filtered from the child. Set "ignore" not to backpropagate. Use list input if you want to set different rewards for each filter step.
+            cut_failed_child: If True, child nodes will be removed when {n_evals * n_tries} evals are filtered.
+            reward_cutoff: Child nodes will be removed if their reward is lower than this value.
             check_loop: If True, duplicate nodes won't be added to the search tree. Should be True if the transition forms a cyclic graph.
             use_dummy_reward: If True, backpropagate value is fixed to 0. (still calculates rewards and objective values)
             
             --- The following variables are provided for ChemTSv2 replication, and are generally recommended to leave at their default values. ---
             failed_parent_reward: Backpropagate this value when {eval_width * n_evals * n_tries} evals are filtered from the node.
-            cut_failed_child: If True, child nodes will be removed when {n_evals * n_tries} evals are filtered.
             freeze_terminal: If True, terminal node won't be visited twice.
             terminal_reward: If "ignore", doesn't backpropagate anything. If float value, backpropagate specified value.
         """
@@ -47,6 +48,7 @@ class MCTS(Generator):
         self.n_evals = n_evals
         self.n_tries = n_tries
         self.cut_failed_child = cut_failed_child
+        self.reward_cutoff = reward_cutoff
         self.terminal_reward = terminal_reward
         self.freeze_terminal = freeze_terminal
         self.check_loop = check_loop
@@ -59,6 +61,9 @@ class MCTS(Generator):
         
     def _selection(self) -> Node:
         node = self.root
+        if not self.root.children and self.root.n > 1:
+            self.logger.info("Search tree exhausted.")
+            raise SystemExit
         while node.children:
             node = self.policy.select_child(node)
             if node.sum_r == -float("inf"): # already exhausted every terminal under this node
@@ -90,6 +95,8 @@ class MCTS(Generator):
         if node.has_reward():
             objective_values, reward = self.get_objective_values_and_reward(node)
             node.reward = reward
+            if self.reward_cutoff is not None and reward < self.reward_cutoff:
+                node.leave()
         else:
             offspring = self.transition.rollout(node)
             objective_values, reward = self.get_objective_values_and_reward(offspring)
