2f41a8925879db6aa77bdbb5dbf0ef8fb489aabc
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-09 13:53:51 +0900

    Add check_no_Hs to SMIRKSTransition

--- transition/smirks.py
+++ transition/smirks.py
@@ -4,12 +4,13 @@ from node import SMILESStringNode
 from transition import Transition
 
 class SMIRKSTransition(Transition):
-    def __init__(self, smirks_list_path: str=None, smirks_list: list[str]=None, check_Hs: bool=True):
+    def __init__(self, smirks_list_path: str=None, smirks_list: list[str]=None, check_no_Hs: bool=True, check_Hs: bool=True):
         """
         Args:
             smirks_list_path: Path to a .txt file containing SMIRKS patterns, one per line. Empty lines and text after '##' are ignored.
             smirks_list: A list of SMIRKS patterns.
-            check_Hs: If True, SMIRKS reactions are also applied to a version of the molecule with explicit hydrogens (via `Chem.AddHs`). Defaults to True.
+            check_no_Hs: If True, SMIRKS reactions are applied to the molecule without explicit hydrogens. Defaults to True.
+            check_Hs: If True, SMIRKS reactions are applied to the molecule with explicit hydrogens (via `Chem.AddHs`). Defaults to True.
         
         Raises:
             ValueError: If both or neither of 'smirks_list_path' and 'smirks_list' are specified.
@@ -22,6 +23,10 @@ class SMIRKSTransition(Transition):
             self.smirks_list = smirks_list
         else:
             raise ValueError("Specify either 'smirks_list_path' or 'smirks_list'.")
+        if not check_no_Hs and not check_Hs:
+            raise ValueError("Set one or both of 'check_no_Hs' or 'check_Hs' to True.")        
+
+        self.check_no_Hs = check_no_Hs
         self.check_Hs = check_Hs
             
     def load_smirks(self, path: str):
@@ -34,17 +39,18 @@ class SMIRKSTransition(Transition):
         
     # implement
     def transitions_with_probs(self, node: SMILESStringNode):
-        initial_mol = node.mol()
+        initial_mol = node.mol(use_cache=False)
         if self.check_Hs:
-            initial_mol_H = Chem.AddHs(initial_mol)
+            initial_mol_with_Hs = Chem.AddHs(initial_mol)
         generated_mols = []
         for smirks in self.smirks_list:
             try:
                 rxn = AllChem.ReactionFromSmarts(smirks)
-
-                products = rxn.RunReactants((initial_mol,))
+                products = []
+                if self.check_no_Hs:
+                    products += rxn.RunReactants((initial_mol,))
                 if self.check_Hs:
-                    products += rxn.RunReactants((initial_mol_H,))
+                    products += rxn.RunReactants((initial_mol_with_Hs,))
                 for ps in products:
                     for p in ps:
                         generated_mols.append((smirks, p))
