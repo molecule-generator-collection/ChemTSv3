3a098166db250962074f42e55c0afe37746686fa
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-09 14:20:06 +0900

    Organize default values

--- filter/charge_filter.py
+++ filter/charge_filter.py
@@ -3,8 +3,8 @@ from rdkit.Chem import Mol
 from filter import MolValueFilter
 
 class ChargeFilter(MolValueFilter):
-    def __init__(self, allowed=0, **kwargs):
-        super().__init__(allowed=allowed, **kwargs)
+    def __init__(self, allowed=0, disallowed=None, max=None, min=None):
+        super().__init__(allowed=allowed, disallowed=disallowed, max=max, min=min)
         
     # implement
     def mol_value(self, mol: Mol) -> int:
--- filter/radical_filter.py
+++ filter/radical_filter.py
@@ -2,8 +2,8 @@ from rdkit.Chem import Descriptors
 from filter import MolValueFilter
 
 class RadicalFilter(MolValueFilter):
-    def __init__(self, allowed=0, **kwargs):
-        super().__init__(allowed=allowed, **kwargs)
+    def __init__(self, allowed=0, disallowed=None, max=None, min=None):
+        super().__init__(allowed=allowed, disallowed=disallowed, max=max, min=min)
         
     # implement
     def mol_value(self, mol) -> int:
