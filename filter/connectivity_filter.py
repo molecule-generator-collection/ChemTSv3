be8873c304b6f05c49e05392d15c065d8824217e
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-09 14:20:06 +0900

    Organize default values

--- filter/connectivity_filter.py
+++ filter/connectivity_filter.py
@@ -1,7 +1,10 @@
 from rdkit.Chem import Mol, rdmolops
-from filter import MolFilter
+from filter import MolValueFilter
 
-class ConnectivityFilter(MolFilter):
+class ConnectivityFilter(MolValueFilter):
+    def __init__(self, allowed=1, disallowed=None, max=None, min=None):
+        super().__init__(allowed=allowed, disallowed=disallowed, max=max, min=min)
+    
     # implement
-    def mol_check(self, mol: Mol) -> bool:
-        return len(rdmolops.GetMolFrags(mol)) == 1
+    def mol_value(self, mol: Mol) -> bool:
+        return len(rdmolops.GetMolFrags(mol))
