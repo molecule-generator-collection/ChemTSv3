cf9be2f9d054d69365ddc14e7b929691c94a9dab
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-10 15:25:40 +0900

    Update for recent get_prior() changes

--- policy/puct.py
+++ policy/puct.py
@@ -6,7 +6,6 @@ class PUCT(UCT):
     # override
     def get_exploration_term(self, node: Node):
         c = self.get_c_value(node)
-        _, prior_weight = self.get_prior(node)
-        n = max(node.n + prior_weight, 1)
-        parent_n = max(node.parent.n + prior_weight, 1)
+        n = max(node.n + self.prior_weight, 1)
+        parent_n = max(node.parent.n + self.prior_weight, 1)
         return c * node.last_prob * sqrt(parent_n) / (1 + n)
