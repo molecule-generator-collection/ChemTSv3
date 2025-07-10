2f06de07987ed35b0e89c915df18d7441528cfb5
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-10 15:09:17 +0900

    Add prior_weight=0 support

--- policy/uct.py
+++ policy/uct.py
@@ -6,6 +6,9 @@ from utils import PointCurve
 
 class UCT(ValuePolicy):
     def __init__(self, c: Callable[[float], float] | list[tuple[float, float]] | float=1, best_rate: float=0.0, prior: float=None, prior_weight: int=1, max_prior: float=None):
+        if prior_weight < 0:
+            raise ValueError("'prior_weight' must be >= 0.")        
+
         if type(c) == Callable:
             self.c = c
         elif type(c) == list:
@@ -18,11 +21,8 @@ class UCT(ValuePolicy):
         self.max_prior = max_prior
         
     def get_prior(self, node: Node) -> float:
-        """Return prior value and prior weight."""
-        if self.prior is not None:
-            return self.prior
-        else:
-            return 0
+        """Return prior value (None if not using prior)."""
+        return self.prior
 
     def get_c_value(self, node: Node) -> float:
         if type(self.c) == Callable:
@@ -35,23 +35,33 @@ class UCT(ValuePolicy):
     
     def get_exploration_term(self, node: Node):
         c = self.get_c_value(node)
-        n = node.n + self.prior_weight
-        parent_n = node.parent.n + self.prior_weight
+        n = max(node.n + self.prior_weight, 1) # prior_weight can be 0
+        parent_n = max(node.parent.n + self.prior_weight, 1)
         return c * sqrt(log(parent_n) / (n))
     
     def get_mean_r(self, node: Node):
         prior = self.get_prior(node)
-        sum_r = node.sum_r + prior * self.prior_weight
-        n = node.n + self.prior_weight
+        if prior is None and node.n == 0:
+            return None
+
+        if prior is None:
+            sum_r = node.sum_r
+        elif self.prior_weight == 0 and node.n == 0:
+            sum_r = prior
+        else:
+            sum_r = node.sum_r + prior * self.prior_weight
+                
+        n = max(node.n + self.prior_weight, 1)
         return sum_r / n
 
     # implement
     def evaluate(self, node: Node) -> float:
-        if node.n == 0:
+        mean_r = self.get_mean_r(node)
+        
+        if mean_r == None:
             return 10**9 # tiebreaker is implemented in policy base
         
         u = self.get_exploration_term(node)
-        mean_r = self.get_mean_r(node)
         best_r = node.best_r
         if self.max_prior is not None:
             best_r = max(self.max_prior, best_r)
