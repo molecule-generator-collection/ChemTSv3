7f6731131faa8b2c13b335b4e6861bacb4f9dded
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-07-15 13:33:34 +0900

    Add default values

--- filter/ring_size_filter.py
+++ filter/ring_size_filter.py
@@ -2,6 +2,9 @@ from rdkit.Chem import Mol, Descriptors
 from filter import MolValueFilter
 
 class MaxRingSizeFilter(MolValueFilter):
+    def __init__(self, max=6, min=None, allowed=None, disallowed=None):
+        super().__init__(allowed=allowed, max=max, min=min, allowed=allowed, disallowed=disallowed)
+        
     # implement
     def mol_value(self, mol: Mol) -> int:
         ri = mol.GetRingInfo()
