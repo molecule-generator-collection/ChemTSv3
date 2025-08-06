b4ec82d22f129296c4edf997a72bd6098126b269
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-08-06 20:25:42 +0900

    Add tutorials for logger and seed

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,5 @@
 from .class_utils import class_from_class_path, class_from_package, camel2snake, add_sep
-from .math_utils import apply_top_p, apply_sharpness, moving_average, max_gauss, min_gauss, rectangular, PointCurve
+from .math_utils import set_seed, apply_top_p, apply_sharpness, moving_average, max_gauss, min_gauss, rectangular, PointCurve
 from .helm_utils import MonomerLibrary, HELMConverter
 from .logging_utils import CSVHandler, NotListFilter, ListFilter, make_logger
 from .mol_utils import is_same_mol, get_main_mol, remove_isotopes, print_atoms_and_labels, draw_mol
