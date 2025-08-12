c3c34bbe0ec31d752842363d18a426b32a2c9c37
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-08-12 17:44:47 +0900

    Simplify prompt arg

--- transition/biot5.py
+++ transition/biot5.py
@@ -4,11 +4,9 @@ from node import MolStringNode
 from transition import BlackBoxTransition
 
 class BioT5Transition(BlackBoxTransition):
-    def __init__(self, target_objective: str, prompt_prefix: str=None, prompt_postfix: str=None, n_samples=2, logger: logging.Logger=None):
-        self.target_objective = target_objective
-        self.prompt_prefix = prompt_prefix or "Definition: You are given a molecule SELFIES. Your job is to generate a SELFIES molecule that"
-        self.prompt_postfix = prompt_postfix or ". Now complete the following example"
-        super().__init__(logger=logger)
+    def __init__(self, prompt: str, n_samples=2, logger: logging.Logger=None):
+        self.prompt = prompt
+        super().__init__(n_samples=n_samples, logger=logger)
         
         self.logger.info("Loading BioT5 models...")
         self.tokenizer = AutoTokenizer.from_pretrained("QizhiPei/biot5-base-text2mol")
@@ -17,7 +15,7 @@ class BioT5Transition(BlackBoxTransition):
         
     def sample_transition(self, node: MolStringNode) -> MolStringNode:
         parent_selfies = node.string
-        prompt = self.prompt_prefix + " " + self.target_objective + self.prompt_postfix + " - Input: <bom>" + parent_selfies + "<eom> Output: "
+        prompt = self.prompt.replace("###SELFIES###", parent_selfies)
         
         input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids
         outputs = self.model.generate(input_ids, max_length=512, do_sample=True)
