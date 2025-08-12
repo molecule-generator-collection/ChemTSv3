246745903788dc267b51ecb85aa51a3a7fd39392
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-08-12 14:43:37 +0900

    Rename NumAtomsFilter to AtomCountFilter

--- filter/__init__.py
+++ filter/__init__.py
@@ -6,7 +6,7 @@ from .connectivity_filter import ConnectivityFilter
 from .hba_filter import HBAFilter
 from .hbd_filter import HBDFilter
 from .log_p_filter import LogPFilter
-from .num_atoms_filter import NumAtomsFilter
+from .atom_count_filter import AtomCountFilter
 from .pains_filter import PainsFilter
 from .radical_filter import RadicalFilter
 from .ring_bond_filter import RingBondFilter
