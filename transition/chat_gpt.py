708a4ce0d1285f18a7b7c0e54680c56eacbef656
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-08-13 13:24:05 +0900

    Add api_key_path arg and multiple prompts support

--- transition/chat_gpt.py
+++ transition/chat_gpt.py
@@ -1,22 +1,38 @@
 import logging
+import os
 from openai import OpenAI
 from node import SMILESStringNode
 from transition import BlackBoxTransition
 
 class ChatGPTTransition(BlackBoxTransition):
-    def __init__(self, api_key: str, prompt: str, model: str="gpt-4o-mini", n_samples=2, logger: logging.Logger=None):
+    def __init__(self, prompt: str, model: str="gpt-4o-mini", api_key: str=None, api_key_path: str=None, n_samples=2, logger: logging.Logger=None):
+        if api_key is None and api_key_path is None:
+            raise ValueError("Specify either 'api_key' or 'api_key_path'.")
+        elif api_key is not None and api_key_path is not None:
+            raise ValueError("Specify one of 'api_key' or 'api_key_path', not both.")
+        elif api_key_path is not None:
+            with open(api_key_path, "r", encoding="utf-8") as f:
+                api_key = f.read().strip()
         self.api_key = api_key
+        
+        if not isinstance(prompt, list):
+            prompt = [prompt]
         self.prompt = prompt
+        
         self.model = model
         super().__init__(n_samples=n_samples, logger=logger)
         
     # implement
     def sample_transition(self, node: SMILESStringNode) -> SMILESStringNode:
         parent_smiles = node.string
-        prompt = self.prompt.replace("###SMILES###", parent_smiles)
         
-        client = OpenAI(api_key=self.api_key)
-        resp = client.responses.create(model=self.model, input=prompt)
-        output_smiles = resp.output_text
+        results = []
+        for p in self.prompt:
+            prompt = p.replace("###SMILES###", parent_smiles)
+            
+            client = OpenAI(api_key=self.api_key)
+            resp = client.responses.create(model=self.model, input=prompt)
+            output_smiles = resp.output_text
+            results.append(SMILESStringNode(string=output_smiles, parent=node))
         
-        return SMILESStringNode(string=output_smiles, parent=node)
+        return results
