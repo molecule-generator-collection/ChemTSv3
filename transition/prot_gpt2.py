e10090ac08048fb3e0ee6b53ae47fbd42fc4041e
satoru-fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-09-09 14:09:45 +0900

    Add eos setting for MolStringNode / Implement fasta generation

--- transition/prot_gpt2.py
+++ transition/prot_gpt2.py
@@ -7,9 +7,10 @@ from transition import Transition
 class ProtGPT2Transition(Transition):
     CANDIDATES = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", # 1~10
              "L", "M", "N", "P", "Q", "R", "S", "T", "U", "V", # 11~20
-             "W", "Y", "Z", "<EOS>"]
+             "W", "Y", "Z"]
     
-    def __init__(self, rollout_top_k: int=950, max_length: int =100, logger: logging.Logger=None):
+    def __init__(self, rollout_top_k: int=950, max_length: int=100, logger: logging.Logger=None):
+        FASTAStringNode.eos = "<EOS>"
         self.protgpt2 = pipeline('text-generation', model="nferruz/ProtGPT2", device="cuda")
         self.rollout_top_k = rollout_top_k
         self._max_length = max_length
@@ -20,20 +21,22 @@ class ProtGPT2Transition(Transition):
         
     def next_nodes(self, node: FASTAStringNode):
         children = []
-        parent = node.string
-        if len(parent) > self.max_length() or parent.endswith("<EOS>"):
+        parent_string = node.string
+        if len(parent_string) > self.max_length() or parent_string.endswith("<EOS>"):
             return []
         
         for a in self.CANDIDATES:
-            new_string = parent + a
-            children.append(FASTAStringNode(new_string, parent=parent, last_prob=1/len(self.CANDIDATES), last_action=a))
+            new_string = parent_string + a
+            children.append(FASTAStringNode(new_string, parent=node, last_prob=1/len(self.CANDIDATES), last_action=a))
         
         return children
     
     def rollout(self, initial_node: FASTAStringNode):
         initial_string = initial_node.string
         
-        raw = self.protgpt2("<|endoftext|>" + initial_string, max_length=self._max_length, do_sample=True, top_k=self.rollout_top_k, num_return_sequences=1, eos_token_id=0)
+        raw = self.protgpt2("<|endoftext|>" + initial_string, max_length=self._max_length, do_sample=True, top_k=self.rollout_top_k, 
+                            num_return_sequences=1, eos_token_id=0, pad_token_id=0)
         s = raw[0]["generated_text"].replace("<|endoftext|>", "").replace("\n", "")
+        s += "<EOS>"
         
         return FASTAStringNode(string=s)
