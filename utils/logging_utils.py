f0287e68af4fc1ffb3c4575b25d81078a18cdeae
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2025-10-15 11:50:30 +0900

    Make delayed logger keep logging level, not overriding info_interval

--- utils/logging_utils.py
+++ utils/logging_utils.py
@@ -1,3 +1,4 @@
+import atexit
 import csv
 from datetime import datetime
 import logging
@@ -41,23 +42,21 @@ def make_logger(output_dir: str, name: str=None, console_level=logging.INFO, fil
     console_handler.setLevel(console_level)
     console_handler.addFilter(NotListFilter())
     file_handler = logging.FileHandler(os.path.join(output_dir, name) + ".log")
+    if delay:
+        file_handler = MemoryHandler(capacity=10000, target=file_handler, flushLevel=logging.CRITICAL)
     file_handler.setLevel(file_level)
     file_handler.addFilter(NotListFilter())
     csv_handler = CSVHandler(os.path.join(output_dir, name) + ".csv")
+    if delay:
+        csv_handler = MemoryHandler(capacity=10000, target=csv_handler, flushLevel=logging.CRITICAL)
     csv_handler.addFilter(ListFilter())
     csv_handler.setLevel(csv_level)
-    
-    if delay:
-        file_handler = MemoryHandler(capacity=10**9,
-                                     target=file_handler,
-                                     flushLevel=logging.CRITICAL)
-        csv_handler = MemoryHandler(capacity=10**9,
-                                    target=csv_handler,
-                                    flushLevel=logging.CRITICAL)
 
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
     logger.addHandler(csv_handler)
+    
+    atexit.register(logging.shutdown)
     
     return logger
 
