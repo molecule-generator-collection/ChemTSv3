51401f54b2209e25eeddb5b348f88c1ad43e1afa
toitataiyo <s202065d@yokohama-cu.ac.jp>
2025-11-18 12:48:42 +0900

    Add OotaChofilter

--- filter/__init__.py
+++ filter/__init__.py
@@ -18,6 +18,7 @@ from .substructure_filter import SubstructureFilter
 from .tpsa_filter import TPSAFilter
 from .validity_filter import ValidityFilter
 from .weight_filter import WeightFilter
+from .oota_cho_filter import OotaChoFilter
 
 # lazy import
 def __getattr__(name):
