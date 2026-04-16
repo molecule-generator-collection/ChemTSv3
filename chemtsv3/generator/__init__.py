49072885804a422b260224d02d8d07465fa2adc3
Satoru Fujii <fujii.sat.rk@yokohama-cu.ac.jp>
2026-04-16 17:39:49 +0900

    Delay imports of MPI-related classes until called

--- chemtsv3/generator/__init__.py
+++ chemtsv3/generator/__init__.py
@@ -2,4 +2,16 @@ from .base import Generator
 from .heapq_generator import HeapQueueGenerator
 from .mcts import MCTS
 from .random_generator import RandomGenerator
-from .async_parallel_mcts import AsyncParallelMCTS, MPIRewardDispatcher, worker_loop
+
+# lazy import
+def __getattr__(name):
+    if name == "AsyncParallelMCTS":
+        from .async_parallel_mcts import AsyncParallelMCTS
+        return AsyncParallelMCTS
+    if name == "MPIRewardDispatcher":
+        from .async_parallel_mcts import MPIRewardDispatcher
+        return MPIRewardDispatcher
+    if name == "AsyncParallelMCTS":
+        from .async_parallel_mcts import worker_loop
+        return worker_loop
+    raise AttributeError(f"module {__name__} has no attribute {name}")
